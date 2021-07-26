985518c23e52c69b00482a2546c2e18336df756c
Jose Felix <86790583+jfelixzr@users.noreply.github.com>
2021-07-25 23:17:26 -0500

    ultimos cambios agregados app imcompleta

---
+++ src/js/constants.js
@@ -0,0 +1,35 @@
+export default {
+    ADD: "add",
+    CATEGORY: "categorySelected",
+    CSS_COMPLETE: "css-complete",
+    DEFAULT: "default",
+    END_TIME: "end-time",
+    FAILED: "failed",
+    HTML_COMPLETE: "html-complete",
+    JS_COMPLETE: "js-complete",
+    FIGMA_COMPLETE: "figma-complete",
+    UX_COMPLETE: "ux-complete",
+    LIFE: 4,
+    MINUTES: "minutes",
+    REMOVE: "remove",
+    RESPONSE: "response",
+    START_TIME: "start-time",
+    SUCCESS: "success",
+    TOTAL_RESPONSES: "total-responses",
+    PROGRESS: "progress",
+    NOTIFICATION: {
+      buttom: "Continuar",
+      type: "default",
+    },
+    NOTIFICATION_FAILED: {
+      buttom: "Continuar",
+      title: "La respuesta correcta es:",
+      type: "failed",
+    },
+    NOTIFICATION_SUCCESS: {
+      buttom: "Continuar",
+      title: "¡Buen Trabajo!",
+      type: "success",
+    },
+  };
+  
