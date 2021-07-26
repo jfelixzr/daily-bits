f23a939558382e3adade716ca15d95f56d0cbdc5
Jose Felix <86790583+jfelixzr@users.noreply.github.com>
2021-07-26 08:31:58 -0500

    updating app incomplete

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+window.onload = ()=>{
+    let home = window.location.href = "/public/home.html";
+    return setTimeout(home, 5000);
+  }
---
+++ src/js/questions.js
@@ -0,0 +1,14 @@
+import thequestions from "./thequestions.js";
+
+
+
+
+
+
+const x=document.querySelector("#close");
+    
+x.addEventListener('click',()=>{
+ 
+  window.location.href = "./home.html";
+});
+  
